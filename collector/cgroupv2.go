af9c6b40b5a1acaf8f03c9ca7ce556d9128b31b7
Cody Eding <edingco@gvsu.edu>
2026-02-06 08:27:50 -0500

    remove ability to collect anything but slurm

--- collector/cgroupv2.go
+++ collector/cgroupv2.go
@@ -37,19 +37,6 @@ var (
 
 func getInfov2(name string, pids []int, metric *CgroupMetric, logger log.Logger) {
 	pathBase := filepath.Base(name)
-	userSlicePattern := regexp.MustCompile("^user-([0-9]+).slice$")
-	userSliceMatch := userSlicePattern.FindStringSubmatch(pathBase)
-	if len(userSliceMatch) == 2 {
-		metric.userslice = true
-		metric.uid = userSliceMatch[1]
-		user, err := user.LookupId(metric.uid)
-		if err != nil {
-			level.Error(logger).Log("msg", "Error looking up user slice uid", "uid", metric.uid, "err", err)
-		} else {
-			metric.username = user.Username
-		}
-		return
-	}
 	slurmPattern := regexp.MustCompile("/job_([0-9]+)(/step_([^/]+)(/user/task_([0-9]+|special))?)?$")
 	slurmMatch := slurmPattern.FindStringSubmatch(name)
 	if len(slurmMatch) == 6 {
