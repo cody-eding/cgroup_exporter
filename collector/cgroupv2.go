4826376e145176ece1c9850848e4993832ab118d
Cody Eding <edingco@gvsu.edu>
2026-02-06 08:28:43 -0500

    remove ability to collect anything but slurm

--- collector/cgroupv2.go
+++ collector/cgroupv2.go
@@ -36,7 +36,6 @@ var (
 )
 
 func getInfov2(name string, pids []int, metric *CgroupMetric, logger log.Logger) {
-	pathBase := filepath.Base(name)
 	slurmPattern := regexp.MustCompile("/job_([0-9]+)(/step_([^/]+)(/user/task_([0-9]+|special))?)?$")
 	slurmMatch := slurmPattern.FindStringSubmatch(name)
 	if len(slurmMatch) == 6 {
